dd8955be249af21c8dbed2951b39bee0de38be03
Leonardo <leonardo.figueiro@yahoo.com.br>
2024-11-27 23:31:57 -0800

    feat: add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,14 @@ export default {
   ],
   theme: {
     extend: {
-
+      colors: {
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        paper: 'var(--color-paper)',
+        primary: 'var(--color-primary)',
+        background: 'var(--color-background)',
+        'primary-contrast': 'var(--color-primary-contrast)',
+      }
     },
   },
   plugins: [],
